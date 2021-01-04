c0171390ca410edb4b899bb4833557c648fe4ccf
balazs4 <balazs4@users.noreply.github.com>
2021-01-04 20:09:05 +0100

    refactor: simplify reduce

--- index.test.js
+++ index.test.js
@@ -1,22 +1,20 @@
 const ago = (date, now = new Date().toJSON()) => {
   return [
-    { mod: 1000, name: "second" },
-    { mod: 1000 * 60, name: "minute" },
-    { mod: 1000 * 60 * 60, name: "hour" },
-    { mod: 1000 * 60 * 60 * 24, name: "day" },
-    { mod: 1000 * 60 * 60 * 24 * 7, name: "week" },
-    { mod: 1000 * 60 * 60 * 24 * 7 * 4, name: "month" },
     { mod: 1000 * 60 * 60 * 24 * 7 * 4 * 12, name: "year" },
-  ]
-    .reduceRight(
-      (times, item) => {
-        const value = Math.floor(times[0] / item.mod);
-        const unit = value === 1 ? item.name : `${item.name}s`;
-        return [...times, `${value} ${unit} ago`];
-      },
-      [new Date(now) - new Date(date)]
-    )
-    .find((x) => /^[1-9]\d* .* ago$/.test(x));
+    { mod: 1000 * 60 * 60 * 24 * 7 * 4, name: "month" },
+    { mod: 1000 * 60 * 60 * 24 * 7, name: "week" },
+    { mod: 1000 * 60 * 60 * 24, name: "day" },
+    { mod: 1000 * 60 * 60, name: "hour" },
+    { mod: 1000 * 60, name: "minute" },
+    { mod: 1000, name: "second" },
+    { mod: 1, name: "millisecond" },
+  ].reduce((io, item) => {
+    if (typeof io === "string") return io;
+    const value = Math.floor(io / item.mod);
+    if (value === 0) return io;
+    const unit = value === 1 ? item.name : `${item.name}s`;
+    return `${value} ${unit} ago`;
+  }, new Date(now) - new Date(date));
 };
 
 const test = require("baretest")("ago");
